9688929dcc170aa7c4625a263343d0885d3414a8
Shaun Bharat <79479981+shaunbharat@users.noreply.github.com>
2025-03-22 14:32:53 -0400

    Treat exact matches of full titles as repeats

--- streamrip/media/artist.py
+++ streamrip/media/artist.py
@@ -117,9 +117,11 @@ class Artist(Media):
         """
         groups: dict[str, list[Album]] = {}
         for a in albums:
-            match = cls._essence_re.match(a.meta.album)
-            assert match is not None
-            title = match.group(1).strip().lower()
+            # match = cls._essence_re.match(a.meta.album)
+            # assert match is not None
+            # title = match.group(1).strip().lower()
+            # todo: temp fix. experimental exact match for repeats
+            title = a.meta.album.strip().lower()
             items = groups.get(title, [])
             items.append(a)
             groups[title] = items
